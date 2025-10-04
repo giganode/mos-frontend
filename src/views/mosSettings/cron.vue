d116234f41cf5373543da6e9d036389e51f3e191
S3ppo <harald@moniholz.at>
2025-10-04 21:52:37 +0200

    Füge Unterstützung für Skripte in Cron-Jobs hinzu: erweitere Dialoge zum Erstellen, Ändern und Löschen von Cron-Jobs und implementiere Skriptverwaltung

--- src/views/mosSettings/cron.vue
+++ src/views/mosSettings/cron.vue
@@ -16,11 +16,26 @@
                 </template>
                 <v-list-item-title>{{ cronJob.name }}</v-list-item-title>
                 <v-list-item-subtitle>{{ cronJob.schedule }} - {{ cronJob.command }}</v-list-item-subtitle>
-                <template v-slot:append>
-                  <v-btn icon @click="openChangeCronJobDialog(cronJob)">
-                    <v-icon>mdi-content-save</v-icon>
-                  </v-btn>
-                </template>
+                    <template v-slot:append>
+                      <v-menu>
+                        <template #activator="{ props }">
+                          <v-btn variant="text" icon v-bind="props">
+                            <v-icon>mdi-dots-vertical</v-icon>
+                          </v-btn>
+                        </template>
+                        <v-list>
+                          <v-list-item @click="openDeleteCronJobDialog(cronJob)">
+                            <v-list-item-title>{{ $t('delete') }}</v-list-item-title>
+                          </v-list-item>
+                          <v-list-item @click="openChangeCronJobDialog(cronJob)">
+                            <v-list-item-title>{{ $t('edit') }}</v-list-item-title>
+                          </v-list-item>
+                          <v-list-item @click="openChangeScriptDialog(cronJob)">
+                            <v-list-item-title>{{ $t('change script') }}</v-list-item-title>
+                          </v-list-item>                          
+                        </v-list>
+                      </v-menu>
+                    </template>
               </v-list-item>
             </v-list>
           </v-card-text>
@@ -35,9 +50,11 @@
       <v-card-title>{{ $t('create cron job') }}</v-card-title>
       <v-card-text>
         <v-form>
+          <v-switch v-model="createCronJobDialog.enabled" :label="$t('enabled')" inset color="primary"></v-switch>
           <v-text-field v-model="createCronJobDialog.name" :label="$t('name')" required></v-text-field>
           <v-text-field v-model="createCronJobDialog.schedule" :label="$t('schedule')" required></v-text-field>
           <v-text-field v-model="createCronJobDialog.command" :label="$t('command')" required></v-text-field>
+          <v-textarea v-model="createCronJobDialog.script" :label="$t('script')" rows="5" required></v-textarea>
         </v-form>
       </v-card-text>
       <v-card-actions>
@@ -54,6 +71,7 @@
       <v-card-title>{{ $t('change cron job') }}</v-card-title>
       <v-card-text>
         <v-form>
+          <v-switch v-model="changeCronJobDialog.enabled" :label="$t('enabled')" inset color="primary"></v-switch>
           <v-text-field v-model="changeCronJobDialog.name" :label="$t('name')" required></v-text-field>
           <v-text-field v-model="changeCronJobDialog.schedule" :label="$t('schedule')" required></v-text-field>
           <v-text-field v-model="changeCronJobDialog.command" :label="$t('command')" required></v-text-field>
@@ -63,6 +81,40 @@
         <v-spacer></v-spacer>
         <v-btn text @click="changeCronJobDialog.value = false">{{ $t('cancel') }}</v-btn>
         <v-btn text @click="changeCronJob()">{{ $t('save') }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Delete Cron Job Dialog -->
+  <v-dialog v-model="deleteCronJobDialog.value" max-width="600px">
+    <v-card>
+      <v-card-title>{{ $t('delete cron job') }} - {{ deleteCronJobDialog.name }}</v-card-title>
+      <v-card-text>
+        <p>{{ $t('are you sure you want to delete the cron job') }}?</p>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="deleteCronJobDialog.value = false">{{ $t('cancel') }}</v-btn>
+        <v-btn text color="red" @click="deleteCronJob()">{{ $t('delete') }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Change Script Dialog -->
+  <v-dialog v-model="changeScriptDialog.value" max-width="800px">
+    <v-card>
+      <v-card-title>{{ $t('change script') }} - {{ changeScriptDialog.name }}</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="changeScriptDialog.path" :label="$t('path')" readonly></v-text-field>
+          <v-text-field v-model="changeScriptDialog.size" :label="$t('size')" readonly></v-text-field>
+          <v-textarea v-model="changeScriptDialog.content" :label="$t('script')" rows="10"></v-textarea>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="changeScriptDialog.value = false">{{ $t('close') }}</v-btn>
+        <v-btn text @click="changeScript()">{{ $t('save') }}</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -79,7 +131,7 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, reactive } from 'vue';
 import { showSnackbarError, showSnackbarSuccess } from '@/composables/snackbar';
 import { useI18n } from 'vue-i18n';
 
@@ -87,18 +139,35 @@ const emit = defineEmits(['refresh-drawer']);
 const cronJobs = ref([]);
 const overlay = ref(false);
 const { t } = useI18n();
-const createCronJobDialog = ref({
+const createCronJobDialog = reactive({
   value: false,
+  enabled: false,
   name: '',
   schedule: '',
-  command: ''
+  command: '',
+  script: ''
 });
-const changeCronJobDialog = ref({
+const changeCronJobDialog = reactive({
   value: false,
+  enabled: false,
   id: '',
   name: '',
   schedule: '',
   command: ''
+});
+const deleteCronJobDialog = reactive({
+  value: false,
+  id: '',
+  name: ''
+});
+const changeScriptDialog = reactive({
+  value: false,
+  name: '',
+  path: '',
+  size: 0,
+  created: '',
+  modified: '',
+  content: ''
 });
 
 onMounted(() => {
@@ -124,7 +193,14 @@ const getCron = async () => {
   }
 };
 
-const createCronJob = async (cronJob) => {
+const createCronJob = async () => {
+  const newCronJob = {
+    name: createCronJobDialog.name,
+    schedule: createCronJobDialog.schedule,
+    command: createCronJobDialog.command,
+    enabled: createCronJobDialog.enabled,
+    script: createCronJobDialog.script
+  };
   try {
     const res = await fetch('/api/v1/cron', {
       method: 'POST',
@@ -132,49 +208,148 @@ const createCronJob = async (cronJob) => {
         'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(cronJob)
+      body: JSON.stringify(newCronJob)
     });
 
-    if (!res.ok) throw new Error(t('cron jobs could not be changed'));
-    showSnackbarSuccess(t('cron jobs changed successfully'));
+    if (!res.ok) throw new Error(t('cron job could not be created'));
+    showSnackbarSuccess(t('cron job created successfully'));
+    createCronJobDialog.value = false;
+    getCron();
 
   } catch (e) {
     showSnackbarError(e.message);
   }
 };
 
-const changeCronJob = async (cronJob) => {
+const changeCronJob = async () => {
+  const changeCronJob = {
+    name: changeCronJobDialog.name,
+    schedule: changeCronJobDialog.schedule,
+    command: changeCronJobDialog.command,
+    enabled: changeCronJobDialog.enabled
+  };
   try {
-    const res = await fetch('/api/v1/cron/' + cronJob.id, {
+    const res = await fetch('/api/v1/cron/' + changeCronJobDialog.id, {
       method: 'PUT',
       headers: {
         'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify(cronJob)
+      body: JSON.stringify(changeCronJob)
     });
 
-    if (!res.ok) throw new Error(t('cron jobs could not be changed'));
-    showSnackbarSuccess(t('cron jobs changed successfully'));
+    if (!res.ok) throw new Error(t('cron job could not be changed'));
+    showSnackbarSuccess(t('cron job changed successfully'));
+    changeCronJobDialog.value = false;
+    getCron();
 
   } catch (e) {
     showSnackbarError(e.message);
   }
 };
 
+const deleteCronJob = async () => {
+  try {
+    const res = await fetch('/api/v1/cron/' + deleteCronJobDialog.id, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
+      }
+    });
+
+    if (!res.ok) throw new Error(t('cron jobs could not be deleted'));
+    showSnackbarSuccess(t('cron job deleted successfully'));
+    deleteCronJobDialog.value = false;
+    getCron();
+
+  } catch (e) {
+    showSnackbarError(e.message);
+  }
+};
+
+const getScript = async (cronJobName) => {
+  try {
+    overlay.value = true;
+    const res = await fetch('/api/v1/cron/scripts/' + encodeURIComponent(cronJobName), {
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
+      }
+    });
+    overlay.value = false;
+    if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error.error || t('script could not be loaded'));
+    }
+    const data = await res.json();
+    return data.script;
+  } catch (e) {
+    overlay.value = false;
+    showSnackbarError(e.message);
+  }
+};
+
+const changeScript = async () => {
+  try {
+    overlay.value = true;
+    const res = await fetch('/api/v1/cron/scripts/' + encodeURIComponent(changeScriptDialog.name), {
+      method: 'PUT',
+      headers: {
+        'Authorization': 'Bearer ' + localStorage.getItem('authToken'),
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ content: changeScriptDialog.content })
+    });
+    overlay.value = false;
+    if (!res.ok) {
+      const error = await res.json();
+      throw new Error(error.error || t('script could not be changed'));
+    }
+    showSnackbarSuccess(t('script changed successfully'));
+    changeScriptDialog.value = false;
+  } catch (e) {
+    overlay.value = false;
+    showSnackbarError(e.message);
+  }
+
+};
+
 const openCreateCronJobDialog = () => {
   createCronJobDialog.value = true;
+  createCronJobDialog.enabled = false;
   createCronJobDialog.name = '';
   createCronJobDialog.schedule = '';
   createCronJobDialog.command = '';
+  createCronJobDialog.script = '';
 };
 
 const openChangeCronJobDialog = (cronJob) => {
   changeCronJobDialog.value = true;
   changeCronJobDialog.id = cronJob.id;
+  changeCronJobDialog.enabled = cronJob.enabled;
   changeCronJobDialog.name = cronJob.name;
   changeCronJobDialog.schedule = cronJob.schedule;
   changeCronJobDialog.command = cronJob.command;
+};
+
+const openDeleteCronJobDialog = (cronJob) => {
+  deleteCronJobDialog.value = true;
+  deleteCronJobDialog.id = cronJob.id;
+  deleteCronJobDialog.name = cronJob.name;
+};
+
+const openChangeScriptDialog = async (cronJob) => {
+  changeScriptDialog.value = true;
+  try {
+    const script = await getScript(cronJob.name);
+    changeScriptDialog.name = script.name;
+    changeScriptDialog.path = script.path;
+    changeScriptDialog.size = script.size;
+    changeScriptDialog.created = script.created;
+    changeScriptDialog.modified = script.modified;
+    changeScriptDialog.content = script.content;
+  } catch (e) {
+    showSnackbarError(e.message);
+  }
 };
 
 </script>

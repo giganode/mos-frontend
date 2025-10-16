ece3840b392c8d6d65644d3c67c486aeca59b1f0
S3ppo <harald@moniholz.at>
2025-10-16 22:03:24 +0200

    Füge Unterstützung für Remote-Mounts hinzu: Implementiere Dialog zur Erstellung, Anzeige und Verwaltung von Remote-Mounts sowie entsprechende Übersetzungen.

--- src/views/remoteMounting.vue
+++ src/views/remoteMounting.vue
@@ -1,34 +1,164 @@
 <template>
   <v-container fluid class="d-flex justify-center">
-    <v-container style="width: 100%; max-width: 1920px;" class="pa-0">
+    <v-container style="width: 100%; max-width: 1920px" class="pa-0">
       <v-container col="12" fluid class="pt-0 pr-0 pl-0 pb-4">
         <h2>{{ $t('remote mounting') }}</h2>
       </v-container>
-      <v-container fluid class="pa-0">
-
-      </v-container>
+      <v-container fluid class="pa-0"></v-container>
     </v-container>
   </v-container>
+
+  <v-dialog v-model="newRemoteDialog.value" max-width="600px">
+    <v-card>
+      <v-card-title>{{ $t('create remote mount') }}</v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field v-model="newRemoteDialog.name" :label="$t('name')" required></v-text-field>
+          <v-select
+            v-model="newRemoteDialog.type"
+            :items="['cifs', 'nfs']"
+            :label="$t('type')"
+            required
+          ></v-select>
+          <v-text-field v-model="newRemoteDialog.server" :label="$t('ip')" required></v-text-field>
+          <v-text-field v-model="newRemoteDialog.share" :label="$t('share')" required></v-text-field>
+          <v-text-field v-model="newRemoteDialog.username" :label="$t('username')"></v-text-field>
+          <v-text-field v-model="newRemoteDialog.password" :label="$t('password')" type="password"></v-text-field>
+          <v-text-field v-model="newRemoteDialog.domain" :label="$t('domain')"></v-text-field>
+          <v-select
+            v-model="newRemoteDialog.version"
+            :items="['1.0', '2.0', '2.1', '3.0', '3.02', '3.1.1', '3.2']"
+            :label="$t('version')"
+            required
+          ></v-select>
+          <v-text-field v-model.number="newRemoteDialog.uid" :label="$t('uid')" type="number" required></v-text-field>
+          <v-text-field v-model.number="newRemoteDialog.gid" :label="$t('gid')" type="number" required></v-text-field>
+          <v-switch v-model="newRemoteDialog.auto_mount" :label="$t('automount')" inset color="primary"></v-switch>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn text @click="newRemoteDialog.value = false">{{ $t('cancel') }}</v-btn>
+        <v-btn @click="createRemote()">{{ $t('create') }}</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <!-- Floating Action Button -->
+  <v-fab color="primary" style="position: fixed; bottom: 32px; right: 32px; z-index: 1000;" size="large" icon
+    @click="openCreateMountDialog()">
+    <v-icon>mdi-plus</v-icon>
+  </v-fab>
 
   <v-overlay :model-value="overlay" class="align-center justify-center">
     <v-progress-circular color="primary" size="64" indeterminate></v-progress-circular>
   </v-overlay>
-
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, reactive } from 'vue';
 import { showSnackbarError, showSnackbarSuccess } from '@/composables/snackbar';
-
+import { useI18n } from 'vue-i18n';
 
 const emit = defineEmits(['refresh-drawer']);
 const overlay = ref(false);
-import { useI18n } from 'vue-i18n';
 const { t } = useI18n();
-
-onMounted(() => {
-
+const remotes = ref([]);
+const newRemoteDialog = reactive({
+  value: false,
+  name: '',
+  type: '',
+  server: '',
+  share: '',
+  username: '',
+  password: '',
+  domain: '',
+  version: '3.0',
+  uid: 500,
+  gid: 500,
+  auto_mount: true,
 });
 
+onMounted(() => {
+  getRemotes();
+});
 
+const openCreateMountDialog = () => {
+  newRemoteDialog.value = true;
+};
+
+const clearRemoteDialog = () => {
+  newRemoteDialog.name = '';
+  newRemoteDialog.type = '';
+  newRemoteDialog.server = '';
+  newRemoteDialog.share = '';
+  newRemoteDialog.username = '';
+  newRemoteDialog.password = '';
+  newRemoteDialog.domain = '';
+  newRemoteDialog.version = '3.0';
+  newRemoteDialog.uid = 500;
+  newRemoteDialog.gid = 500;
+  newRemoteDialog.auto_mount = true;
+};
+
+const getRemotes = async () => {
+  try {
+    const res = await fetch('/api/v1/remotes', {
+      headers: {
+        Authorization: 'Bearer ' + localStorage.getItem('authToken'),
+      },
+    });
+
+    if (!res.ok) {
+      const errorDetails = await res.json();
+      throw new Error(`${t('remote mounts could not be fetched')}|$| ${errorDetails.error || t('unknown error')}`);
+    }
+    remotes.value = await res.json();
+  } catch (e) {
+    const [userMessage, apiErrorMessage] = e.message.split('|$|');
+    showSnackbarError(userMessage, apiErrorMessage);
+  }
+};
+
+const createRemote = async () => {
+  const newRemote = {
+    name: newRemoteDialog.name,
+    type: newRemoteDialog.type,
+    server: newRemoteDialog.server,
+    share: newRemoteDialog.share,
+    username: newRemoteDialog.username,
+    password: newRemoteDialog.password,
+    domain: newRemoteDialog.domain,
+    version: newRemoteDialog.version,
+    uid: newRemoteDialog.uid,
+    gid: newRemoteDialog.gid,
+    auto_mount: newRemoteDialog.auto_mount,
+  };
+
+  overlay.value = true;
+  try {
+    const res = await fetch('/api/v1/remotes', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + localStorage.getItem('authToken'),
+      },
+      body: JSON.stringify({ newRemote }),
+    });
+
+    if (!res.ok) {
+      const errorDetails = await res.json();
+      throw new Error(`${t('remote mount could not be created')}|$| ${errorDetails.error || t('unknown error')}`);
+    }
+    showSnackbarSuccess(t('remote mount created successfully'));
+    getRemotes();
+    newRemoteDialog.value = false;
+    clearRemoteDialog();
+  } catch (e) {
+    const [userMessage, apiErrorMessage] = e.message.split('|$|');
+    showSnackbarError(userMessage, apiErrorMessage);
+  } finally {
+    overlay.value = false;
+  }
+};
 </script>

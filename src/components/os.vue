da3783f9ab065553d85da00cfd743ce8827a04a2
S3ppo <harald@moniholz.at>
2025-10-23 08:30:43 +0200

    style: enhance OS information display with improved text formatting and ellipsis for better readability

--- src/components/os.vue
+++ src/components/os.vue
@@ -6,45 +6,47 @@
     <v-card-text>
       <v-row dense>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.base?.[0]?.os_id">
-          <div class="text-caption text-medium-emphasis">{{ $t('platform') }}</div>
-          <div class="text-body-2">{{ osInfo.base[0].os_id }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('platform') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.base[0].os_id" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis">
+            {{ osInfo.base[0].os_id }}
+          </div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.base?.[0]?.os_name">
-          <div class="text-caption text-medium-emphasis">{{ $t('name') }}</div>
-          <div class="text-body-2">{{ osInfo.base[0].os_name }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('name') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.base[0].os_name" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis">{{ osInfo.base[0].os_name }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.base?.[0]?.os_version">
-          <div class="text-caption text-medium-emphasis">{{ $t('version') }}</div>
-          <div class="text-body-2">{{ osInfo.base[0].os_version }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('version') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.base[0].os_version" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis">{{ osInfo.base[0].os_version }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.hostname">
-          <div class="text-caption text-medium-emphasis">{{ $t('hostname') }}</div>
-          <div class="text-body-2">{{ osInfo.hostname }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('hostname') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.hostname" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis">{{ osInfo.hostname }}</div>
         </v-col>
         <v-col cols="12" v-if="osInfo?.mos"><v-divider></v-divider></v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.version">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos version') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.version }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos version') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.version" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.version }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.channel">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos channel') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.channel }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos channel') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.channel" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.channel }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.build">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos build') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.build }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos build') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.build" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.build }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.running_kernel">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos kernel') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.running_kernel }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos kernel') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.running_kernel" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.running_kernel }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.frontend">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos frontend') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.frontend }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos frontend') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.frontend" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.frontend }}</div>
         </v-col>
         <v-col cols="6" sm="6" md="4" xl="3" v-if="osInfo?.mos?.api">
-          <div class="text-caption text-medium-emphasis">{{ $t('mos api') }}</div>
-          <div class="text-body-2">{{ osInfo.mos.api }}</div>
+          <div class="text-caption text-medium-emphasis"><strong>{{ $t('mos api') }}</strong></div>
+          <div class="text-body-2" :title="osInfo.mos.api" style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">{{ osInfo.mos.api }}</div>
         </v-col>
       </v-row>
     </v-card-text>
